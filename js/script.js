4c4b3f1774ff0a448c5dd9f7b18c74873136d432
Jonah Alvarez <85708581+jonah404@users.noreply.github.com>
2022-05-15 17:59:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,44 @@
 alert("Aplicacion para reservas.")
 
+class cliente{
+    constructor (nombreCompleto, correoElectronico, telefono){
+
+        this.nombreCompleto    = nombreCompleto;
+        this.correoElectronico = correoElectronico;
+        this.telefono          = telefono;
+
+    }
+   
+}
+
+const cliente1 = new cliente(prompt("Ingrese su nombre completo: "), prompt("Ingrese su correo electrónico: "), prompt("Ingrese su número telefónico: "));
+
+    while (cliente1.nombreCompleto == "") {
+        alert("Debe ingresar su nombre completo");
+        cliente1.nombreCompleto = prompt("Ingrese su nombre completo: ");
+}
+
+    while (cliente1.correoElectronico == "") {
+        alert("Debe ingresar su correo electronico");
+        cliente1.correoElectronico = prompt("Ingrese su correo electronico: ");
+}
+
+    while(cliente1.telefono == "" || isNaN(cliente1.telefono)){
+        alert("Debe ingresar su telefono");
+        cliente1.telefono = prompt("Ingrese su telefono: ");
+}
+
+alert("Datos ingresados correctamente");
+
+//Fin primera sección (toma de datos del cliente)
+
 let tipoAlojamiento = prompt("Ingrese el tipo de alojamiento:" + "\n" + "- Carpa" + "\n" + "- Cabaña" + "\n" + "- Casa");
 let cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas que van a hospedarse: "));
 let datoEstadia = parseInt(prompt("Ingrese la cantidad de días que desea hospedarse: "));
+
+//Fin segunda sección (toma de datos de la reserva)
+
+//Comienza funcion de validar datos de reserva
 
 function validar(){
     while(tipoAlojamiento != "Carpa" && tipoAlojamiento != "Cabaña" && tipoAlojamiento != "Casa"){
@@ -29,9 +65,15 @@ function validar(){
         } 
 }
 
+//Fin función de validar datos de reserva
+
+//Comienza función de calcular costos
+
 function calcular(tipoAlojamiento, cantidadPersonas, datoEstadia){ 
     let calculo = (tipoAlojamiento + (cantidadPersonas*50)) * datoEstadia
     let calculoDolar = calculo / 117.09
+    let dolarxDia    = calculoDolar / datoEstadia;
+    let pesosxDia    = calculo / datoEstadia;
     switch (tipoAlojamiento) {
         case 100:
             tipoAlojamiento = "Carpa";
@@ -42,12 +84,16 @@ function calcular(tipoAlojamiento, cantidadPersonas, datoEstadia){
         case 300:
             tipoAlojamiento = "Casa";
     }
-    alert("Detalles de la reserva: \n\nTipo de alojamiento seleccionado: " + tipoAlojamiento + 
-    "\nCantidad de personas: " + cantidadPersonas + "\nEstadía: " + datoEstadia + 
-    " días.\n\nDetalle de costos: \n\nCosto total por día en pesos: " + calculo/datoEstadia + 
-    "\nCosto total por día en dólares: " + calculoDolar/datoEstadia + "\nCosto total en pesos: " + calculo + 
-    "\nCosto total en dólares: " + calculoDolar + "\n\nGracias por su reserva.");
+    alert("Hola " + cliente1["nombreCompleto"]+ ", estos son los detalles de la reserva: \n\nTipo de alojamiento seleccionado: " 
+    + tipoAlojamiento + "\nCantidad de personas: " + cantidadPersonas + "\nEstadía: " + datoEstadia + 
+    " días.\n\nDetalle de costos: \n\nCosto total por día en pesos: " + pesosxDia + 
+    "\nCosto total por día en dólares: " + dolarxDia.toFixed(2) + "\nCosto total en pesos: " + calculo + 
+    "\nCosto total en dólares: " + calculoDolar.toFixed(2) + "\n\nGracias por su reserva.");
 }
+
+//Fin función de calcular costos
+
+//Llamada de las funciones
 
 
 validar(tipoAlojamiento, cantidadPersonas, datoEstadia);
