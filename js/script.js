a1f6f5568838b690e3f93eb7c6a9595b4843cd9b
Jonah Alvarez <85708581+jonah404@users.noreply.github.com>
2022-05-18 19:10:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,69 @@
-alert("Aplicacion para reservas.")
+//Aplicacion para realizar reservas.
 
 class cliente{
     constructor (nombreCompleto, correoElectronico, telefono){
 
-        this.nombreCompleto    = nombreCompleto;
-        this.correoElectronico = correoElectronico;
-        this.telefono          = telefono;
+        this.nombreCompleto    = nombreCompleto.toUpperCase();
+        this.correoElectronico = correoElectronico.toUpperCase();
+        this.telefono          = parseInt(telefono);
 
     }
-   
 }
 
-const cliente1 = new cliente(prompt("Ingrese su nombre completo: "), prompt("Ingrese su correo electrónico: "), prompt("Ingrese su número telefónico: "));
+const clientes = [];
 
-    while (cliente1.nombreCompleto == "") {
-        alert("Debe ingresar su nombre completo");
-        cliente1.nombreCompleto = prompt("Ingrese su nombre completo: ");
+let nombre = prompt("Bienvenido a la aplicación de reservas,\nPor favor ingrese su nombre completo:");
+let correo = prompt("Por favor ingrese su correo electrónico:");
+let tel    = prompt("Ingrese su número telefónico:");
+
+clientes.push(new cliente(nombre, correo, tel));
+
+do{
+    if(nombre == ""){
+        alert("Por favor ingrese su nombre.");
+        nombre = prompt("Bienvenido a la aplicación de reservas,\nPor favor ingrese su nombre completo:");
+        clientes.splice(0,1,nombre);
+        clientes.splice(1,1,correo);
+        clientes.splice(2,1,tel);}
+
+        else if(correo == ""){
+            alert("Por favor ingrese su correo.");
+            correo = prompt("Por favor ingrese su correo electrónico:");
+            clientes.splice(0,1,nombre);
+            clientes.splice(1,1,correo);
+            clientes.splice(2,1,tel);}
+
+        else if((tel == "") || (isNaN(tel))){
+            alert("Por favor ingrese un número de teléfono.");
+            tel = prompt("Ingrese su número telefónico:");
+            clientes.splice(0,1,nombre);
+            clientes.splice(1,1,correo);
+            clientes.splice(2,1,tel);}
+
+        else{
+            alert("Datos ingresados correctamente");
 }
+}while(nombre == "" || correo == "" || tel == "" || isNaN(tel));
 
-    while (cliente1.correoElectronico == "") {
-        alert("Debe ingresar su correo electronico");
-        cliente1.correoElectronico = prompt("Ingrese su correo electronico: ");
-}
-
-    while(cliente1.telefono == "" || isNaN(cliente1.telefono)){
-        alert("Debe ingresar su telefono");
-        cliente1.telefono = prompt("Ingrese su telefono: ");
-}
-
-alert("Datos ingresados correctamente");
+console.log(clientes);
 
 //Fin primera sección (toma de datos del cliente)
+
+
+const alojamiento = [{tipo: "Carpa", tipo: "Cabaña", tipo: "Casa"}];
+
 
 let tipoAlojamiento = prompt("Ingrese el tipo de alojamiento:" + "\n" + "- Carpa" + "\n" + "- Cabaña" + "\n" + "- Casa");
 let cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas que van a hospedarse: "));
 let datoEstadia = parseInt(prompt("Ingrese la cantidad de días que desea hospedarse: "));
 
+
 //Fin segunda sección (toma de datos de la reserva)
 
-//Comienza funcion de validar datos de reserva
+//Validar datos de reserva
 
 function validar(){
+ 
     while(tipoAlojamiento != "Carpa" && tipoAlojamiento != "Cabaña" && tipoAlojamiento != "Casa"){
         alert("La opción de hospedaje " + tipoAlojamiento + " no es correcta, solo puede seleccionar Carpa, Cabaña o Casa, por favor intenta de nuevo.");
             tipoAlojamiento = prompt("Ingrese el tipo de alojamiento:" + "\n" + "- Carpa" + "\n" + "- Cabaña" + "\n" + "- Casa");
@@ -53,17 +76,18 @@ function validar(){
         alert("La cantidad de días que desea hospedarse debe ser mayor a 0, por favor intenta de nuevo.");
             datoEstadia = prompt("La cantidad de días que desea hospedarse debe ser mayor a 0, por favor intenta de nuevo.");
     }
-    switch (tipoAlojamiento) {
-        case "Carpa":
-            tipoAlojamiento = 100;
-            break;
-        case "Cabaña":
-            tipoAlojamiento = 200;
-            break;
-        case "Casa":
-            tipoAlojamiento = 300;
-        } 
-}
+        switch (tipoAlojamiento) {
+            case "Carpa":
+                tipoAlojamiento = 100;
+                break;
+            case "Cabaña":
+                tipoAlojamiento = 200;
+                break;
+            case "Casa":
+                tipoAlojamiento = 300;
+            } 
+        }
+
 
 //Fin función de validar datos de reserva
 
@@ -74,6 +98,8 @@ function calcular(tipoAlojamiento, cantidadPersonas, datoEstadia){
     let calculoDolar = calculo / 117.09
     let dolarxDia    = calculoDolar / datoEstadia;
     let pesosxDia    = calculo / datoEstadia;
+    let xPersonaPesos = pesosxDia / cantidadPersonas;
+    let xPersonaDolar = dolarxDia / cantidadPersonas;
     switch (tipoAlojamiento) {
         case 100:
             tipoAlojamiento = "Carpa";
@@ -84,11 +110,12 @@ function calcular(tipoAlojamiento, cantidadPersonas, datoEstadia){
         case 300:
             tipoAlojamiento = "Casa";
     }
-    alert("Hola " + cliente1["nombreCompleto"]+ ", estos son los detalles de la reserva: \n\nTipo de alojamiento seleccionado: " 
+    alert("Hola " + nombre + ", estos son los detalles de la reserva: \n\nTipo de alojamiento seleccionado: " 
     + tipoAlojamiento + "\nCantidad de personas: " + cantidadPersonas + "\nEstadía: " + datoEstadia + 
     " días.\n\nDetalle de costos: \n\nCosto total por día en pesos: " + pesosxDia + 
-    "\nCosto total por día en dólares: " + dolarxDia.toFixed(2) + "\nCosto total en pesos: " + calculo + 
-    "\nCosto total en dólares: " + calculoDolar.toFixed(2) + "\n\nGracias por su reserva.");
+    "\nCosto total por día en dólares: " + dolarxDia.toFixed(2) + "\nCosto total en pesos: " + calculo + " ( " + xPersonaPesos 
+    + " pesos por persona, por día.) \nCosto total en dólares: " + calculoDolar.toFixed(2) + " ( " + xPersonaDolar.toFixed(2) + 
+    " dólares x persona, por día.) \n\nGracias por su reserva.");
 }
 
 //Fin función de calcular costos
